40eeb0a9545c90ebe1080fd49ee6b3c2ad42cea6
bg5fxp-JF <jacobfecunda@outlook.com>
2023-07-14 12:00:28 +0100

    t5

--- jacob/src/app/constants/constants.tsx
+++ jacob/src/app/constants/constants.tsx
@@ -1,7 +1,7 @@
 export const projData = [
 	{
 		title: "Archie Sharpe",
-		img: "/images/Archie.png",
+		img: "public/images/Archie.png",
 		vid: "/videos/Archie.mp4",
 		tech: ["React JS", "Bootstrap CSS"],
 	},
